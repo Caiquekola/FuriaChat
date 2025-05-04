ed35369f8af31650d9348298db63489289d7a5b8
caiquekola <caiquekola@hotmail.com>
2025-05-04 11:20:27 -0300

    FrontEnd Addapt

--- BackEnd/src/main/java/com/furia/fanchat/repository/GameStatusRepository.java
+++
@@ -1,8 +0,0 @@
-package com.furia.fanchat.repository;
-
-import com.furia.fanchat.model.GameStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameStatusRepository extends JpaRepository<GameStatus, Long> {
-    GameStatus findFirstByOrderByIdDesc();
-}
